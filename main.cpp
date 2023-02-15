187982e73b13b9d0337bdf1577ba22c9b0683115
ABEL K ANTONY <abelkantony98@gmail.com>
2023-02-15 17:09:37 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Student
+{
+private:
+	int rollNumber;
+	int  mark[5];
+	int totalMarks=0;
+public:
+	void InputData()
+	{
+		cout<<"\nEnter Roll Number : ";
+		cin>>rollNumber;
+		cout<<"\nEnter marks \n";
+		for (int i=0; i<5; i++)
+			cin>>mark[i];
+	}
+	void GetTotalMarks()
+	{
+		for (int i =0; i<5;i++)
+			totalMarks += mark[i];
+		cout<<totalMarks;
+	}
+};
+
+
+int main()
+{
+	Student stu;
+	stu.InputData();
+	stu.GetTotalMarks();
+}
